a2a8aa618fe2e066d06825373fed3a7a8d3c0b8e
emanuel <emanuelmeloa@gmail.com>
2026-05-20 22:54:13 -0300

    alterando o main

---
+++ src/main.py
@@ -0,0 +1,23 @@
+from numpy import array as ar
+from numpy.linalg import norm
+from numpy import sum as soma_np
+
+n, e, p = input("Digite três valores separados por espaço: ").split()
+n = int(n)
+e = int(e)
+p = int(p)
+
+lista_cordenadas = [ list(map(float,input().split())) for i in range(n)]
+
+print(f"{lista_cordenadas[0]}")
+
+lista_np = ar(lista_cordenadas)
+
+array_pesos = []
+for i in range(0,n+1):
+    if i+1>= n: break
+    distancia = norm(lista_np[i]- lista_np[i+1])
+    array_pesos.append(distancia)
+print(array_pesos)
+array_pesos = soma_np(array_pesos)
+print("soma das distancias", array_pesos)
