2c821949702e0750cbc4ecb165a9369d2fa6622b
Emanuel Alves Melo <emanuelmeloa@gmail.com>
2026-05-24 12:25:29 -0300

    Implement Prim's algorithm for minimum spanning tree
    Refactor distance calculation and graph creation using EdgeWeightedGraph and PrimMST.

--- src/main.py
+++ src/main.py
@@ -1,23 +1,42 @@
-from numpy import array as ar
-from numpy.linalg import norm
-from numpy import sum as soma_np
+import math
 
-n, e, p = input("Digite três valores separados por espaço: ").split()
+from edge import Edge
+from edge_weighted_graph import EdgeWeightedGraph
+from prim_mst import PrimMST
+
+n, e, p = input("").split()
 n = int(n)
 e = int(e)
 p = int(p)
 
 lista_cordenadas = [ list(map(float,input().split())) for i in range(n)]
 
-print(f"{lista_cordenadas[0]}")
 
-lista_np = ar(lista_cordenadas)
 
-array_pesos = []
-for i in range(e,n+1):
-    if i+1> n: break
-    distancia = norm(lista_np[i-1]- lista_np[i])
-    array_pesos.append(distancia)
-print(array_pesos)
-array_pesos = soma_np(array_pesos)
-print("soma das distancias", array_pesos)
+matriz_pesos = [[0 for _ in range(n)] for _ in range(n)]
+for i in range(n):
+    for j in range(i+1, n):
+        if i<e and j<e:
+            distancia = 0
+        else:
+            distancia = math.dist(lista_cordenadas[i], lista_cordenadas[j])
+        matriz_pesos[i][j] = distancia
+        matriz_pesos[j][i] = distancia
+for i in range(p):
+    v, w = input("").split()
+    v = int(v)-1
+    w = int(w)-1
+    matriz_pesos[v][w] = 0
+    matriz_pesos[w][v] = 0
+
+
+graph = EdgeWeightedGraph(n)
+for i in range(n):
+    for j in range(i+1, n):
+        if i != j:
+            edge = Edge(i, j, float(matriz_pesos[i][j]))
+            graph.add_edge(edge)
+mst = PrimMST(graph)
+print("%.6f" % mst.weight())
+
+bp=1
